aa0405de63d209003eabc81245e2a742697a34e8
Clifford Sharp <csharpusa@gmail.com>
2023-01-30 16:49:03 -0600

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Cliff Sharp on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
